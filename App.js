c3ff99da23aef1b5cc39afa4f216bbad4cc23eb2
robocon321 <robocon321n@gmail.com>
2021-07-08 09:03:32 +0700

    Menu for TodayUI

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from "react-native-paper";
 import LeftNavigation from './components/LeftNavigation';
 import Inbox from './screens/Inbox';
 import Today from './screens/Today';
@@ -10,17 +11,19 @@ import Today from './screens/Today';
 export default function App() {
   const Drawer = createDrawerNavigator();
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Drawer.Navigator 
-              drawerStyle={{width:"85%"}}
-              initialRouteName="Today"  
-              drawerContent={props => <LeftNavigation {...props} />}>
-          <Drawer.Screen name="Today" component={Today}/>
-          <Drawer.Screen name="Inbox" component={Inbox}/>
-        </Drawer.Navigator>
-      </NavigationContainer>
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <NavigationContainer>
+          <Drawer.Navigator 
+                drawerStyle={{width:"85%"}}
+                initialRouteName="Today"  
+                drawerContent={props => <LeftNavigation {...props} />}>
+            <Drawer.Screen name="Today" component={Today}/>
+            <Drawer.Screen name="Inbox" component={Inbox}/>
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </View>
+    </Provider>
   );
 }
 
