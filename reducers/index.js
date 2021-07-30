32e056f50d5d42cf9d0bf00d5a3d1a652b24a506
robocon321 <robocon321n@gmail.com>
2021-07-30 22:16:06 +0700

    Add combine reducer

--- reducers/index.js
+++ reducers/index.js
@@ -3,10 +3,12 @@ import labelReducer from './label';
 import projectReducer from './project';
 import taskReducer from './task';
 import labelTaskReducer from './labelTask';
+import commentTaskReducer from './commentTask';
 
 export default combineReducers({
   labels: labelReducer,
   projects: projectReducer,
   tasks: taskReducer,
   labelTasks: labelTaskReducer,
+  commentTask: commentTaskReducer,
 });
